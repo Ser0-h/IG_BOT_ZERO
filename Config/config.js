98b348868cd804c6f1dbd7bf8bbdda617b8fc8ba
Ser0-h <einahmed99@gmail.com>
2026-09-12 17:26:20 +0600

    Create config.js

---
+++ Config/config.js
@@ -0,0 +1,48 @@
+{
+  "botName": "IG-BOT V1",
+  "description": "Instagram command bot",
+  "author": "",
+  "website": "",
+  "github": "",
+
+  "commandsPath": "./commands",
+  "eventsPath": "./events",
+  "languagesPath": "./languages",
+  "language": "en",
+  "prefix": "!",
+
+  "adminIds": [],
+  "ownerId": "",
+  "allowThreadAdmins": true,
+  "allowedThreads": [],
+  "blockedThreads": [],
+
+  "autoReply": false,
+  "welcomeMessages": true,
+  "autoMarkRead": false,
+
+  "logLevel": "info",
+  "maxHistory": 50,
+
+  "pollIntervalMs": 5000,
+  "commandCooldownMs": 1500,
+  "cooldown": {
+    "windowMs": 10000,
+    "max": 5
+  },
+
+  "maxHandlerEntries": 2000,
+  "maxCooldownEntries": 10000,
+  "maxTrackedThreads": 10000,
+  "maxUsers": 10000,
+  "maxThreads": 5000,
+
+  "dataDir": "./data",
+  "stateFile": "./data/bot-state.json",
+  "accountFile": "./accounts/account.txt",
+
+  "chatApi": {
+    "url": "",
+    "token": ""
+  }
+}
